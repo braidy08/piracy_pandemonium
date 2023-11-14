3468207ae2e96ac434b64be21d93d65d8170fa72
braidy08 <braidy08isme@gmail.com>
2023-11-14 19:20:11 +1100

    i feel like my add.xml is filling up because im too lazy to remove assets

--- hooks/local_strings.lua
+++ hooks/local_strings.lua
@@ -1,4 +1,4 @@
---yes, I stole this from Cargoship Raid
+--yes i stole this from Cargoship Raid
 Hooks:Add("LocalizationManagerPostInit", "PPaltdayLocalization", function(loc)
 		LocalizationManager:add_localized_strings({
 		["hud_assault_assault"] = "ESCAPE",
