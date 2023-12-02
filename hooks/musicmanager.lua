fed26509a4a5e7e62f41819ca531a75bedba25dd
braidy08 <braidy08isme@gmail.com>
2023-12-02 12:42:28 +1100

    woah

--- hooks/musicmanager.lua
+++ hooks/musicmanager.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(MusicManager, "jukebox_default_tracks", "_piracy_pandemonium_track", function(self)
 	local track = Hooks:GetReturn()
-	track.heist_piracy_pandemonium_nar_name = "track_71"
+	track.heist_piracy_pandemonium_name = "track_71"
 	return track
 end)
