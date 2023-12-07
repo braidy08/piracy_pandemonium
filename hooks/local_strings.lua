915de71f21001343c68022bb0ea10ca04e1b3f44
braidy08 <braidy08isme@gmail.com>
2023-12-07 20:26:06 +1100

    i forgor to publish this

--- hooks/local_strings.lua
+++ hooks/local_strings.lua
@@ -2,5 +2,6 @@
 Hooks:Add("LocalizationManagerPostInit", "PPaltdayLocalization", function(loc)
 		LocalizationManager:add_localized_strings({
 		["hud_assault_assault"] = "ESCAPE",
+		["hud_hint_blame_missing"] = "",
 		})
 end)
