ef3a8a448481c528236c8006f9f846adaedc181a
braidy08 <braidy08@duck.com>
2025-05-08 15:02:57 +1000

    a

--- hooks/tweakdatapd2.lua
+++ hooks/tweakdatapd2.lua
@@ -13,11 +13,11 @@ Hooks:PostHook(TweakData, "_setup_point_of_no_returns", "priacy_pandemonium_ponr
 		scale_box = true,
 		text_id = "hud_assault_point_no_PP_OMGGETTHEHELLOUT"
 	}
-	self.point_of_no_returns.PP_RussiansExplodeSafehouse = {
+	self.point_of_no_returns.PP_ExplodeSafehouse = {
 		color = Color(1, 1, 0, 0),
 		timer_flash_color = Color(1, 1, 0.8, 0.2),
 		attention_color = Color(1, 1, 1, 1),
 		scale_box = true,
-		text_id = "hud_assault_point_no_PP_RussiansExplodeSafehouse"
+		text_id = "hud_assault_point_no_PP_ExplodeSafehouse"
 	}
 end)
